225b5c6611a6aee6718f63a35e67d35ca70bd377
mcseagate <mcseagate@gmail.com>
2018-12-30 20:48:02 -0800

    new_user

--- src/test/java/Testcases/RegistrationTest.java
+++ src/test/java/Testcases/RegistrationTest.java
@@ -1,14 +1,18 @@
 package Testcases;
 
 import PageElements.PageRegistrationElements;
+import Screenshots.Screenshot;
 import TestBase.BaseClass;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class RegistrationTest extends BaseClass {
     PageRegistrationElements PRE;
@@ -31,14 +35,14 @@ public class RegistrationTest extends BaseClass {
     }
 
 
-    @Test
+    @Test(priority = 1)
     public void userRegistration(){
 
         PRE.registration();
         test.log(LogStatus.INFO, "Registration Completed Successfully");
     }
 
-    @Test
+    @Test(priority = 2)
     public void verifyTitle(){
         String title = driver.getTitle();
         System.out.println(title);
@@ -49,7 +53,13 @@ public class RegistrationTest extends BaseClass {
 
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown(ITestResult testResult) throws IOException {
+        if(testResult.getStatus()==ITestResult.FAILURE){
+            String path = Screenshot.takeScreenshots(driver, testResult.getName());
+            String imagePath = test.addScreenCapture(path);
+            test.log(LogStatus.FAIL, "Test Failed");
+
+        }
         driver.quit();
         test.log(LogStatus.INFO, "Test Ended Successfully");
         report.endTest(test);
